6d2c1c05bfdd5aecf19a6e8d822866154b475cea
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2018-10-23 22:57:33 +0530

    Revert "Add unit tests for OpenmrsUtil.join"
    This reverts commit ba40c4a64e7a51e1242239790a9eba4af24c4f12.

--- api/src/test/java/org/openmrs/util/OpenmrsUtilUnitTest.java
+++ api/src/test/java/org/openmrs/util/OpenmrsUtilUnitTest.java
@@ -12,15 +12,11 @@ package org.openmrs.util;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -29,12 +25,6 @@ import org.junit.Test;
  */
 public class OpenmrsUtilUnitTest {
 
-	private List<String> moduleList;
-
-	@Before
-	public void setUp() {
-		moduleList = Arrays.asList("module1", "module2");
-	}
 
 	@Test
 	public void parseParameterList_shouldReturnKeyPairsGivenParametersSeparatedByPipe() {
@@ -98,43 +88,5 @@ public class OpenmrsUtilUnitTest {
 	public void parseParameterList_shouldFailGivenOnlyKeysSeparatedByPipes() {
 
 		OpenmrsUtil.parseParameterList("role_id|patient_id");
-	}
-
-	@Test
-	public void join_shouldReturnEmptyStringGivenNullForCollection() {
-		
-		assertThat(OpenmrsUtil.join(null, ""), is(""));
-	}
-	
-	@Test
-	public void join_shouldReturnEmptyStringGivenEmptyCollection() {
-
-		assertThat(OpenmrsUtil.join(Collections.EMPTY_LIST, ""), is(""));
-	}
-
-	@Test
-	public void join_shouldReturnListElementsJoinedBySeparatorWithoutLeadingOrTrailingSeparator() {
-
-		assertThat(OpenmrsUtil.join(moduleList, ","), is("module1,module2"));
-	}
-	
-	@Test
-	public void join_shouldReturnListElementsJoinedBySeparatorIfGivenCollectionContainsNull() {
-
-		List<String> listWithNull = Arrays.asList("module1", null, "module2");
-
-		assertThat(OpenmrsUtil.join(listWithNull, ","), is("module1,null,module2"));
-	}
-
-	@Test
-	public void join_shouldReturnListElementsJoinedBySeparatorIfGivenSeparatorIsEmptyString() {
-
-		assertThat(OpenmrsUtil.join(moduleList, ""), is("module1module2"));
-	}
-
-	@Test
-	public void join_shouldReturnListElementsJoinedBySeparatorIfGivenSeparatorIsNull() {
-
-		assertThat(OpenmrsUtil.join(moduleList, null), is("module1nullmodule2"));
 	}
 }
