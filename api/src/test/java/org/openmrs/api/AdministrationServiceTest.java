7514b37e058996e0c624a446edd7944d9d88c13f
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2018-10-23 22:57:33 +0530

    Revert "TRUNK-5417 AdministrationServiceTest should count global properties correctly (#2702)"
    This reverts commit ef8bbfebcc07f845c9d6b6995bbe7a6fc0361130.

--- api/src/test/java/org/openmrs/api/AdministrationServiceTest.java
+++ api/src/test/java/org/openmrs/api/AdministrationServiceTest.java
@@ -367,9 +367,8 @@ public class AdministrationServiceTest extends BaseContextSensitiveTest {
 	
 	@Test
 	public void getAllGlobalProperties_shouldReturnAllGlobalPropertiesInTheDatabase() {
-		int allGlobalPropertiesSize = adminService.getAllGlobalProperties().size();
 		executeDataSet(ADMIN_INITIAL_DATA_XML);
-		assertEquals(allGlobalPropertiesSize + 9, adminService.getAllGlobalProperties().size());
+		assertEquals(21, adminService.getAllGlobalProperties().size());
 	}
 	
 	@Test
@@ -411,9 +410,10 @@ public class AdministrationServiceTest extends BaseContextSensitiveTest {
 	@Test
 	public void purgeGlobalProperty_shouldDeleteGlobalPropertyFromDatabase() {
 		executeDataSet(ADMIN_INITIAL_DATA_XML);
-		int allGlobalPropertiesSize = adminService.getAllGlobalProperties().size();
+		
+		assertEquals(21, adminService.getAllGlobalProperties().size());
 		adminService.purgeGlobalProperty(adminService.getGlobalPropertyObject("a_valid_gp_key"));
-		assertEquals(allGlobalPropertiesSize -1, adminService.getAllGlobalProperties().size());
+		assertEquals(20, adminService.getAllGlobalProperties().size());
 	}
 	
 	@Test
