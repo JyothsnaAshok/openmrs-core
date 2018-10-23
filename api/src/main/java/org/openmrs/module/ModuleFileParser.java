5730b804aa3e586fe2fc66f5bc442b2c6c6a11d4
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2018-10-23 22:57:33 +0530

    Revert "TRUNK-5385 Use try-with-resource on JarFile"
    This reverts commit 6f7566d5353a7f8d474f21398766dc84e1938504.

--- api/src/main/java/org/openmrs/module/ModuleFileParser.java
+++ api/src/main/java/org/openmrs/module/ModuleFileParser.java
@@ -126,9 +126,18 @@ public class ModuleFileParser {
 	 */
 	public Module parse() throws ModuleException {
 		
+		JarFile jarfile = null;
 		Document configDoc;
 
-		try (JarFile jarfile = new JarFile(moduleFile)) {
+		try {
+			try {
+				jarfile = new JarFile(moduleFile);
+			}
+			catch (IOException e) {
+				throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.cannotGetJarFile"),
+				        moduleFile.getName(), e);
+			}
+
 			// look for config.xml in the root of the module
 			ZipEntry config = jarfile.getEntry("config.xml");
 			if (config == null) {
@@ -144,9 +153,13 @@ public class ModuleFileParser {
 				    "Module.error.cannotGetConfigFileStream"), moduleFile.getName(), e);
 			}
 		}
-		catch (IOException e) {
-			throw new ModuleException(Context.getMessageSourceService().getMessage("Module.error.cannotGetJarFile"),
-				moduleFile.getName(), e);
+		finally {
+			try {
+				jarfile.close();
+			}
+			catch (Exception e) {
+				log.warn("Unable to close jarfile: " + jarfile, e);
+			}
 		}
 		return createModule(configDoc);
 	}
