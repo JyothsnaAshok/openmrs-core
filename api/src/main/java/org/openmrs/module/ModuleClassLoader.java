47ed504d40405c334810d103fa5d4129175ee943
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2018-10-23 22:57:33 +0530

    Revert "GCI-239 Check is listFiles null before looping (#2466)"
    This reverts commit 9443fd4d4f17ae3b4f68f9a0cd818e87f8215873.

--- api/src/main/java/org/openmrs/module/ModuleClassLoader.java
+++ api/src/main/java/org/openmrs/module/ModuleClassLoader.java
@@ -112,10 +112,7 @@ public class ModuleClassLoader extends URLClassLoader {
 		File devDir = ModuleUtil.getDevelopmentDirectory(module.getModuleId());
 		if (devDir != null) {
 			File[] fileList = devDir.listFiles();
-			if (fileList == null) {
-				return;
-			}
-			for (File file : fileList) {
+			for (File file : Objects.requireNonNull(fileList)) {
 				if (!file.isDirectory()) {
 					continue;
 				}
@@ -210,16 +207,14 @@ public class ModuleClassLoader extends URLClassLoader {
 		try {
 			if (devDir != null) {
 				File[] fileList = devDir.listFiles();
-				if (fileList != null) {
-					for (File file : fileList) {
-						if (!file.isDirectory()) {
-							continue;
-						}
-						File dir = new File(devDir, file.getName() + File.separator + "target" + File.separator + "classes" + File.separator);
-						if (dir.exists()) {
-							result.add(dir.toURI().toURL());
-							devFolderNames.add(file.getName());
-						}
+				for (File file : Objects.requireNonNull(fileList)) {
+					if (!file.isDirectory()) {
+						continue;
+					}
+					File dir = new File(devDir, file.getName() + File.separator + "target" + File.separator + "classes" + File.separator);
+					if (dir.exists()) {
+						result.add(dir.toURI().toURL());
+						devFolderNames.add(file.getName());
 					}
 				}
 			}
