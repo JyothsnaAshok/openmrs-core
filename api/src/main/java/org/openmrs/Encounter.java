ddbd137cd714a98f0b5d9d38f2a75f564ba00bdd
JyothsnaAshok <34093139+JyothsnaAshok@users.noreply.github.com>
2018-10-23 22:57:33 +0530

    Revert "TRUNK-5339: Introduce a new Diagnosis domain object (#2576)"
    This reverts commit 88f109b26f80eaefbc2e6cfbe24814805aaeb686.

--- api/src/main/java/org/openmrs/Encounter.java
+++ api/src/main/java/org/openmrs/Encounter.java
@@ -53,9 +53,7 @@ public class Encounter extends BaseChangeableOpenmrsData {
 	private EncounterType encounterType;
 	
 	private Set<Order> orders;
-
-	private Set<Diagnosis> diagnoses;
-
+	
 	@AllowDirectAccess
 	private Set<Obs> obs;
 	
@@ -385,26 +383,6 @@ public class Encounter extends BaseChangeableOpenmrsData {
 	 */
 	public void setPatient(Patient patient) {
 		this.patient = patient;
-	}
-
-	/**
-	 * Gets the set of diagnoses
-	 * @return diagnoses - the set of diagnoses.
-	 * 
-	 *  @since 2.2
-	 */
-	public Set<Diagnosis> getDiagnoses() {
-		return diagnoses;
-	}
-
-	/**
-	 * Sets a set of diagnoses for the current Encounter
-	 * @param diagnoses the set of Diagnosis to set.
-	 * 
-	 * @since 2.2   
-	 */
-	public void setDiagnoses(Set<Diagnosis> diagnoses) {
-		this.diagnoses = diagnoses;
 	}
 	
 	/**
